afd81c84a2e51f98336c733c3a548c0ac78be2ec
Conor McCarter <conormccarter@gmail.com>
2026-08-17 22:11:36 -0400

    chore: update to v0.2.1

--- Formula/loonfs.rb
+++ Formula/loonfs.rb
@@ -1,17 +1,17 @@
 class Loonfs < Formula
   desc "Durable filesystem built on object storage"
   homepage "https://github.com/loonfs/loonfs"
-  url "https://github.com/loonfs/loonfs/releases/download/v0.2.0/loonfs-aarch64-apple-darwin.tar.gz"
-  sha256 "cd04e642b56123b8f9349de1b71290aa705f19957704a3e882b7cbc460826ba0"
+  url "https://github.com/loonfs/loonfs/releases/download/v0.2.1/loonfs-aarch64-apple-darwin.tar.gz"
+  sha256 "b00f015fbc1fa65079f4b16ff167af59b1251e0d5695a0f3db8b85acce8e0746"
   license "Apache-2.0"
 
   depends_on :macos
 
   on_macos do
     on_intel do
-      url "https://github.com/loonfs/loonfs/releases/download/v0.2.0/loonfs-x86_64-apple-darwin.tar.gz"
-      sha256 "f384679f8626098e6d93b403c4f2af0fd01e963f91d3fa2949e5023729da136f"
-      version "0.2.0"
+      url "https://github.com/loonfs/loonfs/releases/download/v0.2.1/loonfs-x86_64-apple-darwin.tar.gz"
+      sha256 "2af5fcc53bdb4234f7eda1d79ee4d01e44e60462d25cbc64fbfa21b2d012c481"
+      version "0.2.1"
     end
   end
 
